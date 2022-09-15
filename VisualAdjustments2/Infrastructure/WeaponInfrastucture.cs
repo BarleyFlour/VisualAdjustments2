db0ed7ac77c3290e5298ad16cc5ec56d0955ab71
Barleyflour <noah.schlaucher@gmail.com>
2022-09-15 09:34:08 +0200

    Fixed hide belt items

--- VisualAdjustments2/Infrastructure/WeaponInfrastucture.cs
+++ VisualAdjustments2/Infrastructure/WeaponInfrastucture.cs
@@ -15,6 +15,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kingmaker.Blueprints.Items.Weapons;
+using Kingmaker.UI.Common;
 using Kingmaker.Visual.Particles.FxSpawnSystem;
 using UnityEngine;
 
@@ -24,7 +25,8 @@ namespace VisualAdjustments2.Infrastructure
     // public class WeaponInfrastucture
     //  {
     //[HarmonyPatch(typeof(UnitViewHandSlotData), "VisibleItemBlueprint", MethodType.Getter)]
-    [HarmonyPatch(typeof(UnitViewHandSlotData), nameof(UnitViewHandSlotData.VisibleItemVisualParameters), MethodType.Getter)]
+    [HarmonyPatch(typeof(UnitViewHandSlotData), nameof(UnitViewHandSlotData.VisibleItemVisualParameters),
+        MethodType.Getter)]
     public static class UnitViewHandsSlotData_VisibleItemBlueprint_Patch
     {
         public static void Postfix(UnitViewHandSlotData __instance, ref WeaponVisualParameters __result)
@@ -36,11 +38,16 @@ namespace VisualAdjustments2.Infrastructure
                 if (characterSettings == null) return;
                 if (__instance.VisibleItem == null) return;
                 {
-                    var bp = ((BlueprintItemEquipmentHand)__instance.VisibleItem.Blueprint)?.VisualParameters?.AnimStyle.ToString();
-                    var WeaponOverride = characterSettings.WeaponOverrides.FirstOrDefault(b => b.Slot == __instance.Owner.View.HandsEquipment?.Sets?.Keys?.ToList().IndexOf(__instance?.Slot?.HandsEquipmentSet) && b?.AnimStyle == bp && __instance?.m_IsMainHand == b?.MainOrOffHand);
+                    var bp = ((BlueprintItemEquipmentHand)__instance.VisibleItem.Blueprint)?.VisualParameters?.AnimStyle
+                        .ToString();
+                    var WeaponOverride = characterSettings.WeaponOverrides.FirstOrDefault(b =>
+                        b.Slot == __instance.Owner.View.HandsEquipment?.Sets?.Keys?.ToList()
+                            .IndexOf(__instance?.Slot?.HandsEquipmentSet) && b?.AnimStyle == bp &&
+                        __instance?.m_IsMainHand == b?.MainOrOffHand);
                     if (WeaponOverride != null)
                     {
-                        __result = ResourcesLibrary.TryGetBlueprint<BlueprintItemEquipmentHand>(WeaponOverride.GUID).VisualParameters;
+                        __result = ResourcesLibrary.TryGetBlueprint<BlueprintItemEquipmentHand>(WeaponOverride.GUID)
+                            .VisualParameters;
                     }
                 }
             }
@@ -50,6 +57,7 @@ namespace VisualAdjustments2.Infrastructure
             }
         }
     }
+
     [HarmonyPatch(typeof(UnitViewHandSlotData), nameof(UnitViewHandSlotData.UpdateWeaponEnchantmentFx))]
     public static class UnitViewHandSlotData_UpdateWeaponEnchantmentFX_Patch
     {
@@ -78,27 +86,34 @@ namespace VisualAdjustments2.Infrastructure
                     {
                         FxHelper.Destroy(fxObject);
                     }
+
                     characterSettings.CurrentFXs[__instance.Slot.MaybeItem].Clear();
                 }
+
                 if (__instance.IsInHand && isVisible)
                 {
-                    if (!characterSettings.CurrentFXs.ContainsKey(__instance.Slot.MaybeItem)) characterSettings.CurrentFXs[__instance.Slot.MaybeItem] = new List<IFxHandle>();
+                    if (!characterSettings.CurrentFXs.ContainsKey(__instance.Slot.MaybeItem))
+                        characterSettings.CurrentFXs[__instance.Slot.MaybeItem] = new List<IFxHandle>();
                     foreach (var enchantOverride in characterSettings.EnchantOverrides)
                     {
-                        if ((__instance.Owner.View.HandsEquipment?.Sets?.Keys?.ToList().IndexOf(__instance?.Slot?.HandsEquipmentSet) == enchantOverride.Slot) && __instance.Slot.IsPrimaryHand == enchantOverride.MainOrOffHand)
+                        if ((__instance.Owner.View.HandsEquipment?.Sets?.Keys?.ToList()
+                                .IndexOf(__instance?.Slot?.HandsEquipmentSet) == enchantOverride.Slot) &&
+                            __instance.Slot.IsPrimaryHand == enchantOverride.MainOrOffHand)
                         {
                             foreach (var enchant in __instance.m_EnchantmentFxObjects.ToTempList())
                             {
                                 enchant.HandleDestroy();
-                               // GameObject.Destroy(enchant);
+                                // GameObject.Destroy(enchant);
                                 __instance.m_EnchantmentFxObjects.Remove(enchant);
                             }
+
                             if (enchantOverride.GUID == "Hide")
                             {
-
                                 continue;
                             }
-                            var blueprint = ResourcesLibrary.TryGetBlueprint<BlueprintWeaponEnchantment>(enchantOverride.GUID);
+
+                            var blueprint =
+                                ResourcesLibrary.TryGetBlueprint<BlueprintWeaponEnchantment>(enchantOverride.GUID);
                             if (blueprint == null || blueprint.WeaponFxPrefab == null) continue;
                             var fxObject = RespawnFx(blueprint.WeaponFxPrefab.Load(), __instance.Slot.MaybeItem);
                             characterSettings.CurrentFXs[__instance.Slot.MaybeItem].Add(fxObject);
@@ -112,7 +127,8 @@ namespace VisualAdjustments2.Infrastructure
             }
         }
     }
-    [HarmonyPatch(typeof(DollRoom), "UpdateAvatarRenderers")]
+
+    [HarmonyPatch(typeof(DollRoom), nameof(DollRoom.UpdateAvatarRenderers))]
     public static class DollRoom_UpdateAvatarRenderers_Patchf
     {
         //static FastInvoker<DollRoom, GameObject, object> UnscaleFxTimes;
@@ -122,7 +138,8 @@ namespace VisualAdjustments2.Infrastructure
             return true;
         }*/
 
-        private static void Postfix(DollRoom __instance, UnitViewHandsEquipment ___m_AvatarHands, UnitEntityData ___m_Unit)
+        private static void Postfix(DollRoom __instance, UnitViewHandsEquipment ___m_AvatarHands,
+            UnitEntityData ___m_Unit)
         {
             try
             {
@@ -132,18 +149,19 @@ namespace VisualAdjustments2.Infrastructure
                 if (characterSettings == null) return;
                 foreach (var isOffhand in new bool[] { true, false })
                 {
-                    WeaponParticlesSnapMap weaponParticlesSnapMap = ___m_AvatarHands?.GetWeaponModel(isOffhand)?.GetComponent<WeaponParticlesSnapMap>();
+                    WeaponParticlesSnapMap weaponParticlesSnapMap = ___m_AvatarHands?.GetWeaponModel(isOffhand)
+                        ?.GetComponent<WeaponParticlesSnapMap>();
                     if (weaponParticlesSnapMap)
                     {
                         UnityEngine.Object x = weaponParticlesSnapMap;
-                        UnityEngine.Object y = isOffhand ?
-                            ___m_Unit?.Body?.SecondaryHand.FxSnapMap :
-                            ___m_Unit?.Body?.PrimaryHand.FxSnapMap;
+                        UnityEngine.Object y = isOffhand
+                            ? ___m_Unit?.Body?.SecondaryHand.FxSnapMap
+                            : ___m_Unit?.Body?.PrimaryHand.FxSnapMap;
                         if (x == y)
                         {
-                            var weapon = isOffhand ?
-                                    ___m_Unit?.Body?.SecondaryHand?.MaybeItem :
-                                    ___m_Unit?.Body?.PrimaryHand?.MaybeItem;
+                            var weapon = isOffhand
+                                ? ___m_Unit?.Body?.SecondaryHand?.MaybeItem
+                                : ___m_Unit?.Body?.PrimaryHand?.MaybeItem;
                             characterSettings.CurrentFXs.TryGetValue(weapon, out List<IFxHandle> fxObjects);
                             if (fxObjects != null)
                             {
@@ -162,5 +180,33 @@ namespace VisualAdjustments2.Infrastructure
             }
         }
     }
-    //}
+
+    [HarmonyPatch(typeof(UnitViewHandsEquipment), nameof(UnitViewHandsEquipment.UpdateBeltPrefabs))]
+    static class UnitViewHandsEquipment_UpdateBeltPrefabs_Patch
+    {
+        private static void Postfix(UnitViewHandsEquipment __instance, GameObject[] ___m_ConsumableSlots)
+        {
+            try
+            {
+#if DEBUG
+                Main.Logger.Log($"Updating {__instance.Owner.CharacterName}'s Belt items");
+#endif
+                if (!__instance.Owner.IsPlayerFaction) return;
+#if DEBUG
+                Main.Logger.Log($"{__instance.Owner.CharacterName} is of PlayerFaction");
+#endif
+                // Main.logger.Log("UpdateBeltPrefabs");
+                var characterSettings = __instance.Owner.GetSettings();
+                if (characterSettings.HideEquipmentDict[(ItemsFilter.ItemType)11])
+                {
+                    Main.Logger.Log($"tried to hide {__instance.Owner.CharacterName}'s belt items");
+                    foreach (var go in ___m_ConsumableSlots) go?.SetActive(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Main.Logger.Error(ex.ToString());
+            }
+        }
+    }
 }
--- VisualAdjustments2/UI/PCViews/New/HideEquipmentButtonPCView.cs
+++ VisualAdjustments2/UI/PCViews/New/HideEquipmentButtonPCView.cs
@@ -24,51 +24,65 @@ namespace VisualAdjustments2.UI
             switch (type)
             {
                 case HideButtonType.Class_Equipment:
-                    {
-                        settings.HideEquipmentDict[(ItemsFilter.ItemType)type] = state;
-                        Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value.Value?.View?.CharacterAvatar.UpdateBackpackVisibility(state);
-                        break;
-                    }
+                {
+                    settings.HideEquipmentDict[(ItemsFilter.ItemType)type] = state;
+                    Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value.Value?.View?.CharacterAvatar
+                        .UpdateBackpackVisibility(state);
+                    break;
+                }
                 case HideButtonType.Mythic_Things:
+                {
+                    settings.HideEquipmentDict[(ItemsFilter.ItemType)type] = state;
+                    if (state == true)
                     {
-                        settings.HideEquipmentDict[(ItemsFilter.ItemType)type] = state;
-                        if(state == true)
-                        {
-                            Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value.Value?.View?.CharacterAvatar?.SetAdditionalVisualSettings(null);
-                            Kingmaker.Game.Instance.UI?.Common?.DollRoom?.m_Avatar?.SetAdditionalVisualSettings(null);
-                           // Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value?.Unit?.View?.CharacterAvatar.ApplyAdditionalVisualSettings();
-                           // Kingmaker.Game.Instance.UI?.Common?.DollRoom?.m_Avatar?.ApplyAdditionalVisualSettings();
-                        }
-                        else
-                        {
-                            var charclass = Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value.Value.Progression?.GetVisualSettingsProvider()?.GetAdditionalVisualSettings();
-                            Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value.Value?.View?.CharacterAvatar?.SetAdditionalVisualSettings(charclass);
-                            Kingmaker.Game.Instance.UI?.Common?.DollRoom?.m_Avatar?.SetAdditionalVisualSettings(charclass);
-                           // Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value?.Unit?.View?.CharacterAvatar.ApplyAdditionalVisualSettings();
-                           // Kingmaker.Game.Instance.UI?.Common?.DollRoom?.m_Avatar?.ApplyAdditionalVisualSettings();
-                        }
+                        Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value.Value?.View?.CharacterAvatar
+                            ?.SetAdditionalVisualSettings(null);
+                        Kingmaker.Game.Instance.UI?.Common?.DollRoom?.m_Avatar?.SetAdditionalVisualSettings(null);
+                        // Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value?.Unit?.View?.CharacterAvatar.ApplyAdditionalVisualSettings();
+                        // Kingmaker.Game.Instance.UI?.Common?.DollRoom?.m_Avatar?.ApplyAdditionalVisualSettings();
+                    }
+                    else
+                    {
+                        var charclass = Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value.Value
+                            .Progression?.GetVisualSettingsProvider()?.GetAdditionalVisualSettings();
+                        Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value.Value?.View?.CharacterAvatar
+                            ?.SetAdditionalVisualSettings(charclass);
+                        Kingmaker.Game.Instance.UI?.Common?.DollRoom?.m_Avatar?.SetAdditionalVisualSettings(charclass);
+                        // Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value?.Unit?.View?.CharacterAvatar.ApplyAdditionalVisualSettings();
+                        // Kingmaker.Game.Instance.UI?.Common?.DollRoom?.m_Avatar?.ApplyAdditionalVisualSettings();
+                    }
 
-                        break;
-                    }
+                    break;
+                }
+                case HideButtonType.Usable:
+                {
+                    settings.HideEquipmentDict[(ItemsFilter.ItemType)type] = state;
+                    Kingmaker.Game.Instance?.SelectionCharacter?.CurrentSelectedCharacter?.View?.HandsEquipment?.UpdateBeltPrefabs();
+                    Kingmaker.Game.Instance.UI?.Common?.DollRoom?.AvatarHands?.UpdateBeltPrefabs();
+                    break;
+                }
                 default:
-                    {
-                        settings.HideEquipmentDict[(ItemsFilter.ItemType)type] = state;
-                        break;
-                    }
+                {
+                    settings.HideEquipmentDict[(ItemsFilter.ItemType)type] = state;
+                    break;
+                }
             }
+
             Kingmaker.Game.Instance?.SelectionCharacter?.CurrentSelectedCharacter?.View?.VAUpdate();
         }
+
         public void SetupFromVisualSettingsView(CharacterVisualSettingsEntityPCView old)
         {
             this.m_Toggle = old.m_Button;
             this.m_Label = old.m_Label;
             UnityEngine.Component.DestroyImmediate(old);
         }
+
         bool GetCheckState()
         {
             var settings = Kingmaker.Game.Instance?.SelectionCharacter?.SelectedUnit?.Value.Value?.GetSettings();
             if (settings == null) return false;
-            if(settings.HideEquipmentDict.TryGetValue((ItemsFilter.ItemType)type,out var value))
+            if (settings.HideEquipmentDict.TryGetValue((ItemsFilter.ItemType)type, out var value))
             {
                 return value;
             }
@@ -77,8 +91,9 @@ namespace VisualAdjustments2.UI
                 settings.HideEquipmentDict.Add((ItemsFilter.ItemType)type, false);
                 return false;
             }
-           // return settings.HideEquipmentDict[(ItemsFilter.ItemType)type];
+            // return settings.HideEquipmentDict[(ItemsFilter.ItemType)type];
         }
+
         public void Bind(string label, HideButtonType buttonType)
         {
             this.type = buttonType;
@@ -90,24 +105,24 @@ namespace VisualAdjustments2.UI
             {
                 onChangeStateDisposable.Dispose();
             }
+
             IDisposable onChangeVisualDisposable = this.m_OnChangeVisualDisposable;
             if (onChangeVisualDisposable != null)
             {
                 onChangeVisualDisposable.Dispose();
             }
-            this.IsOnState.Value = this.GetCheckState();
-            this.m_OnChangeVisualDisposable = this.IsOnState.Subscribe(delegate (bool state)
-            {
 
+            this.IsOnState.Value = this.GetCheckState();
+            this.m_OnChangeVisualDisposable = this.IsOnState.Subscribe(delegate(bool state)
+            {
                 //Action onChangeStateAction = this.m_OnChangeStateAction;
                 //if (onChangeStateAction != null)
                 {
-
                     SetOption(state);
                 }
                 this.m_Toggle.SetActiveLayer(state ? 1 : 0);
             });
-            this.m_OnChangeStateDisposable = this.m_Toggle.OnLeftClickAsObservable().Subscribe(delegate (Unit _)
+            this.m_OnChangeStateDisposable = this.m_Toggle.OnLeftClickAsObservable().Subscribe(delegate(Unit _)
             {
                 this.IsOnState.Value = !this.IsOnState.Value;
             });
