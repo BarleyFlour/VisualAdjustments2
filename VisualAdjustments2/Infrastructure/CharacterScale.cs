4cceb59b4fc45958f721178dd5e8e825f6a09027
BarleyFlour <82173938+BarleyFlour@users.noreply.github.com>
2023-11-21 18:03:02 +0100

    v1.2.6 Fixed for game version 2.2.0as

--- VisualAdjustments2/Infrastructure/CharacterScale.cs
+++ VisualAdjustments2/Infrastructure/CharacterScale.cs
@@ -11,7 +11,7 @@ namespace VisualAdjustments2.Infrastructure
 {
     public class CharacterScale
     {
-        [HarmonyPatch(typeof(UnitEntityView), nameof(UnitEntityView.GetSizeScale))]
+        [HarmonyPatch(typeof(UnitEntityView), nameof(UnitEntityView.GetSizeScale), argumentTypes: new Type[]{typeof(Size)} ) ]
         private static class UnitEntityView_GetSizeScale_Patch
         {
             private static void Postfix(UnitEntityView __instance, ref float __result)
@@ -24,6 +24,39 @@ namespace VisualAdjustments2.Infrastructure
                     if (!characterSettings.BuffSettings.FixSize) return;
                     //Size originalSize = __instance.EntityData.Descriptor.OriginalSize;
                    // Size size = __instance.EntityData.Descriptor.State.Size;
+                    __result = 1;
+                    /*if (__instance.DisableSizeScaling) //Used when polymorphed
+                    {
+                        originalSize = size;
+                    }
+                    float sizeDiff = characterSettings.overrideScaleAdditive ?
+                        ((int)size + characterSettings.additiveScaleFactor - (int)originalSize) :
+                       (characterSettings.overrideScaleFactor - (int)originalSize);
+                    float sizeScale = Mathf.Pow(1 / 0.66f, sizeDiff);
+                    __result = sizeScale;*/
+                }
+                catch (Exception ex)
+                {
+                    Main.Logger.Error(ex.ToString());
+                }
+            }
+        }
+    }
+    public class CharacterScale2
+    {
+        [HarmonyPatch(typeof(UnitEntityView), nameof(UnitEntityView.GetSizeScale), argumentTypes: new Type[]{} ) ]
+        private static class UnitEntityView_GetSizeScale2_Patch
+        {
+            private static void Postfix(UnitEntityView __instance, ref float __result)
+            {
+                try
+                {
+                    if (__instance.EntityData == null) return;
+                    if (!__instance.EntityData.IsPlayerFaction) return;
+                    var characterSettings = __instance.EntityData.GetSettings();
+                    if (!characterSettings.BuffSettings.FixSize) return;
+                    //Size originalSize = __instance.EntityData.Descriptor.OriginalSize;
+                    // Size size = __instance.EntityData.Descriptor.State.Size;
                     __result = 1;
                     /*if (__instance.DisableSizeScaling) //Used when polymorphed
                     {
